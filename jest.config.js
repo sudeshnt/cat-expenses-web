a973fe2c1e00f17d44245d322950e4d41e53bfd9
Sudesh Nimesha <sudeshnt93@gmail.com>
2023-09-17 00:09:36 +0700

    fixed build errors

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
+  preset: "ts-jest",
+  transform: {
+    "^.+\\.(ts|tsx)?$": "ts-jest",
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
 };
