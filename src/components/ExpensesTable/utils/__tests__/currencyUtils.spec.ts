5bb5ab4b8d8a33bb49f3807b33bb3029573f8da0
Sudesh Nimesha <sudeshnt93@gmail.com>
2023-09-16 23:09:20 +0700

    added unit tests

---
+++ src/components/ExpensesTable/utils/__tests__/currencyUtils.spec.ts
@@ -0,0 +1,26 @@
+import { numberToUSD } from "../currencyUtils";
+
+describe("numberToUSD", () => {
+  it("formats a positive number to USD currency format", () => {
+    expect(numberToUSD(1000)).toBe("$1,000.00");
+    expect(numberToUSD(500.5)).toBe("$500.50");
+  });
+
+  it("formats a negative number to USD currency format", () => {
+    expect(numberToUSD(-1000)).toBe("-$1,000.00");
+    expect(numberToUSD(-500.5)).toBe("-$500.50");
+  });
+
+  it("formats zero to USD currency format", () => {
+    expect(numberToUSD(0)).toBe("$0.00");
+  });
+
+  it("handles very large numbers", () => {
+    expect(numberToUSD(1e10)).toBe("$10,000,000,000.00");
+  });
+
+  it("handles decimal precision", () => {
+    expect(numberToUSD(123.456789)).toBe("$123.46"); // Rounded to 2 decimal places
+    expect(numberToUSD(123.454)).toBe("$123.45"); // Rounded to 2 decimal places
+  });
+});
